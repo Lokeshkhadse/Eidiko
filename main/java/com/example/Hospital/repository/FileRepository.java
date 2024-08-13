dc4617326d4811f425762507c1e9cf13695e09df
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2024-08-13 06:01:08 -0700

    Delete main directory

--- main/java/com/example/Hospital/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Hospital.repository;
-
-import com.example.Hospital.entity.FileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileEntity , Long> {
-}
--- main/java/com/example/Hospital/repository/HospitalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Hospital.repository;
-
-import com.example.Hospital.entity.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital , Long> {
-}

99a1610fc297ea1182513b629f3dfe8f017a65c1
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2024-09-04 06:28:10 -0700

    Add files via upload

---
+++ ReadMail/src/test/java/com/example/ReadMail/ReadMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReadMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
